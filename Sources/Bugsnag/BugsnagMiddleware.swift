cc62269f5c81c17d7c4018dd0ffefaf3edb9be6b
tanner0101 <me@tanner.xyz>
2020-06-26 16:21:48 -0400

    Finish adding Bugsnag docs

--- Sources/Bugsnag/BugsnagMiddleware.swift
+++ Sources/Bugsnag/BugsnagMiddleware.swift
@@ -1,10 +1,16 @@
 import Vapor
 
+/// Automatically catches and reports errors to Bugsnag.
+///
+///     app.middleware.use(BugsnagMiddleware())
+///
+/// This should be placed _before_ `ErrorMiddleware`. 
 public struct BugsnagMiddleware {
     public init() { }
 }
 
 extension BugsnagMiddleware: Middleware {
+    /// See `Middleware`.
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
         next.respond(to: request).flatMapErrorThrowing { error in
             request.bugsnag.report(error)
--- Sources/Bugsnag/Severity.swift
+++ Sources/Bugsnag/Severity.swift
@@ -1,6 +1,12 @@
+/// Error severity. See `BugsnagError`.
 public struct BugsnagSeverity {
+    /// Information.
     public static let info = Self(value: "info")
+
+    /// Warning.
     public static let warning = Self(value: "warning")
+
+    /// Error.
     public static let error = Self(value: "error")
 
     let value: String
