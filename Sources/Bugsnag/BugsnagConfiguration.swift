992f7bc28a16d39a9c4002efa18fc75c41a8bbad
tanner0101 <me@tanner.xyz>
2020-06-26 16:09:11 -0400

    Finish adding Bugsnag docs

--- Sources/Bugsnag/BugsnagConfiguration.swift
+++ Sources/Bugsnag/BugsnagConfiguration.swift
@@ -1,12 +1,24 @@
+/// Bugsnag's configuration options. 
+///
+///     app.bugsnag.configuration = .init(...)
+///
 public struct BugsnagConfiguration {
+    /// Notifier API key found in Bugsnag project settings.
     public var apiKey: String
+
+    /// Which version of your app is running, like `development` or `production`.
     public var releaseStage: String
     
     /// A version identifier, (eg. a git hash)
     public var version: String?
+
+    /// Defines sensitive keys that should be hidden from data reported to Bugsnag.
     public var keyFilters: Set<String>
+
+    /// Controls whether reports are sent to Bugsnag.
     public var shouldReport: Bool
 
+    /// Creates a new `BugsnagConfiguration`.
     public init(
         apiKey: String,
         releaseStage: String,
